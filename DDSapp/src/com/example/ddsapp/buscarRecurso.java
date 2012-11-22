f7848b9320f162028e24fed3ead8f4bf8c593494
Diego Carey <diegocarey@gmail.com>
2012-11-22 17:04:18 -0300

    Proyecto AppManager y Aplicacion

--- DDSapp/src/com/example/ddsapp/buscarRecurso.java
+++ DDSapp/src/com/example/ddsapp/buscarRecurso.java
@@ -1,5 +1,10 @@
 package com.example.ddsapp;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cl.puc.dds.appmgr.external.IResource;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +17,19 @@ public class buscarRecurso extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buscar_recurso_activity);
         getIntent();
+        
+        MyApplication app = new MyApplication();
+        
+        ArrayList<IResource> ListaRecursos = app.getAllForeignResources();
+        String[] ListaTipoRecurso = new String[ListaRecursos.size()];
+        int j = 0;
+        
+        for(Iterator<IResource> i = ListaRecursos.iterator(); i.hasNext(); ) {
+        	  IResource resource = i.next();
+        	  String type = resource.getType();
+        	  ListaTipoRecurso[j] = type;
+        	  j++;
+        	}
         
         
     }
